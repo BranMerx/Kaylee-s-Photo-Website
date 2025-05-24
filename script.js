389b451acdcdcf12f07205ea5eecbeb4ee3c8146
BranMerx <bmercadofranco@oakland.edu>
2025-05-23 22:17:13 -0400

    Beginning work on JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-//Event listener for the language buttons
+//Event listener for the spanish language buttons
 document.getElementById('Language Selection').addEventListener('click', function(){
   //Change the index html element to the selected language
+  window.location.href = "index_es.html";
    
+});
+
+//Event listener for english language buttons
+document.getElementById('Seleccion de idioma').addEventListener('click', function(){
+//Change the index_es to index.html
+  //Change the index html element to the selected language
+  //window.location.href = "index.html";
+  window.location.href = "index.html";
 });
