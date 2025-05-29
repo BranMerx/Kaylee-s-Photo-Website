bd8db98724a594bfaebba28d02d47eb9f67f6df6
BranMerx <bmercadofranco@oakland.edu>
2025-05-28 22:29:50 -0400

    Language Buttons

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-//Event listener for the spanish language buttons
-document.getElementById('Language Selection').addEventListener('click', function(){
+//Event listener for the spanish language button
+document.getElementById('español').addEventListener('click', function(){
   //Change the index html element to the selected language
   window.location.href = "index_es.html";
    
 });
 
-//Event listener for english language buttons
-document.getElementById('Seleccion de idioma').addEventListener('click', function(){
+//Event listener for english language button
+document.getElementById('english').addEventListener('click', function(){
 //Change the index_es to index.html
-  //Change the index html element to the selected language
-  //window.location.href = "index.html";
   window.location.href = "index.html";
 });
