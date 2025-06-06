ea18dbf61a2c4bae363398cba96739401c5ef363
BranMerx <bmercadofranco@oakland.edu>
2025-06-05 20:51:07 -0400

    Spanish Javascript Button updates

--- script_es.js
+++ script_es.js
@@ -1,4 +1,14 @@
 document.getElementById('english').addEventListener('click', function(){
-//Change the index_es to index.html
   window.location.href = "index.html";
+});
+
+//event listern for the photo upload spanish version
+document.getElementById('subirButton').addEventListener('click', function(){
+  //Access the phone camera
+});
+
+//event listener to view the collage
+document.getElementById('vistaButon').addEventListener('click', function(){
+  //Change the index html element to the collage view
+  window.location.href = "photo_collage.html";
 });
