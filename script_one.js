af3da60fff5d0159cd86413051bb37345dc6bd36
BranMerx <bmercadofranco@oakland.edu>
2025-06-08 21:26:30 -0400

    Singular JS not working

--- script_one.js
+++ script_one.js
@@ -2,11 +2,12 @@ document.getElementById('español').addEventListener('click', toSpanish);
 document.getElementById('english').addEventListener('click', toEnglish);
 
 //function to change to english
-function toEnglish() {
-  window.location.href = "index.html";
-}
 //function to change to spanish
 function toSpanish() {
   window.location.href = "index_es.html";
+}
+
+function toEnglish() {
+  window.location.href = "index.html";
 }
 
