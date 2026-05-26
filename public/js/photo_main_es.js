24a7017dc3e63a728edf15d89adf0a5ed32e1711
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-05-26 18:51:15 -0400

    Attempt to fix return button on spanish page

--- public/js/photo_main_es.js
+++ public/js/photo_main_es.js
@@ -1,44 +1,6 @@
-const photoGrid = document.getElementById('photoGrid');
+const photoGrid = document.getElementById('photoGrid_es');
 
 document.getElementById('returnButton_es').addEventListener('click', function(){
   window.location.href = "index_es.html";
 });
 
-/*
-function addPhotoToGrid(photoUrl, fullName) {
-  const gridItem = document.createElement('div');
-  gridItem.className = 'grid-item';
-
-  const img = document.createElement('img');
-  img.src = photoUrl;
-
-  const name = document.createElement('p');
-  name.textContent = fullName;
-
-  gridItem.appendChild(img);
-  gridItem.appendChild(name);
-  photoGrid.appendChild(gridItem);
-}
-
-
-// Function to fetch and display photos from the server
-async function fetchPhotos() {
-  try {
-    const response = await fetch('https://kaylee-s-photo-website.onrender.com/photos');
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const photos = await response.json();
-
-    photos.forEach(photo => {
-      const fullName = `${photo.FirstName} ${photo.LastName}`;
-      addPhotoToGrid(photo.S3url, fullName);
-    }); 
-  } catch (error) {
-    console.error('Error fetching photos:', error);
-  }
-}
-
-// Call the function to fetch and display photos when the page loads
-fetchPhotos();
-*/
