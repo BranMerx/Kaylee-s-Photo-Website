43fd41fd2290f2f6edd5a49d77a543b4d8ca4649
Brandon Mercado Franco <brandon.mercadofranco@gmail.com>
2026-06-02 21:09:19 -0400

    Changing Picture Display

--- public/js/photo_main.js
+++ public/js/photo_main.js
@@ -3,3 +3,19 @@ const photoGrid = document.getElementById('photoGrid');
 document.getElementById('returnButton').addEventListener('click', function(){
   window.location.href = "index.html";
 });
+
+//lightbox functionality
+const galleryImages = document.querySelectorAll('.grid-item img');
+
+galleryImages.forEach(image => {
+  image.addEventListener('click', function() {
+    document.getElementById('lightbox').style.display = 'flex';
+    document.getElementById('lightbox').querySelector('img').src = this.src;
+  });
+});
+
+document.getElementById('lightbox').addEventListener('click', function() {
+  this.style.display = 'none';
+});
+
+  
